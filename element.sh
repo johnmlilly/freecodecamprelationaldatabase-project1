909f6a1b0faf6cad34796a463ebe8fa1a6392d30
John Lilly <83787644+johnmlilly@users.noreply.github.com>
2026-05-21 14:35:12 -0400

    Upload periodic table files

---
+++ element.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
+
+# Determine if input is number, symbol, or name
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  CONDITION="atomic_number=$1"
+else
+  CONDITION="symbol='$1' OR name='$1'"
+fi
+
+RESULT=$($PSQL "
+SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
+FROM elements e
+JOIN properties p USING(atomic_number)
+JOIN types t USING(type_id)
+WHERE $CONDITION;
+")
+
+if [[ -z $RESULT ]]
+then
+  echo "I could not find that element in the database."
+else
+  IFS="|" read ATOMIC NAME SYMBOL TYPE MASS MELTING BOILING <<< "$RESULT"
+
+  echo "The element with atomic number $ATOMIC is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+fi
